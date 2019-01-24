4bec72560a35f106fb5edb238754c87b85feade6
Karunakar reddy Katasani <31701720+S530472@users.noreply.github.com>
2019-01-24 16:34:01 -0600

    Add ReversingAnArray.java

---
+++ RavenTraining/src/JavaPractice/ReversingAnArray.java
@@ -0,0 +1,17 @@
+package JavaPractice;
+
+public class ReversingAnArray {
+public static void main(String args[]) {
+int a[]= {12,54,36,85,69,74,25,20};
+int s=a.length;
+int c;
+for(int i=0;i<s/2;i++) {
+	c=a[i];
+	a[i]=a[(s-1)-i];
+	a[(s-1)-i]=c;
+}
+for(int y=0;y<a.length;y++) {
+	System.out.print(a[y]+"\t");
+}
+}
+}

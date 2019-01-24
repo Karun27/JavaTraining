e32065874d48d825928b934dfd80486acc0c401f
Karunakar reddy Katasani <31701720+S530472@users.noreply.github.com>
2019-01-24 12:37:33 -0600

    Add PrintingPatterns.java

---
+++ RavenTraining/src/JavaPractice/PrintingPatterns.java
@@ -0,0 +1,15 @@
+package JavaPractice;
+
+public class PrintingPatterns {
+	public static void main(String args[]) {
+
+		String a ="*";
+		for(int i=0;i<=5;i++) {
+			for(int j=0;j<i;j++) {
+			System.out.print(a+" ");
+		}
+			System.out.println();
+	}
+
+}
+}
